085f494d1f3fb33a22eadd3a67f0dbb6b85748d5
vlj2007 <vlj2007@yandex.ru>
2023-07-26 19:16:35 +0700

    I_have_finished_task2

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,33 @@
 public class Main {
     public static void main(String[] args) {
         task1();
+        task2();
     }
-    public static void task1(){
+
+    public static void task1() {
         System.out.println("Задача 1");
         int save = 15_000;
         int accumulate = 0;
         int months = 0;
-        while (accumulate < 2_459_000){
+        while (accumulate < 2_459_000) {
             accumulate = accumulate + save;
             months += 1;
-            System.out.println("Месяц "+ months + ", сумма накоплений равна " + accumulate +  " рублей");
+            System.out.println("Месяц " + months + ", сумма накоплений равна " + accumulate + " рублей");
         }
+    }
+
+    public static void task2() {
+        System.out.println("Задача 2");
+        int n = 0;
+        int m = 10;
+        while (n < 10) {
+            n++;
+            System.out.print(n + " ");
+        }
+        System.out.println();
+        for (; m > 0; m--) {
+            System.out.print(m + " ");
+        }
+
     }
 }
