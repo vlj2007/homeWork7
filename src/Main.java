fa8594ee46310cd184b57070af6c6ae33f4d48a7
vlj2007 <vlj2007@yandex.ru>
2023-07-26 18:40:00 +0700

    I_have_finished_task1

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,13 @@ public class Main {
     }
     public static void task1(){
         System.out.println("Задача 1");
+        int save = 15_000;
+        int accumulate = 0;
+        int months = 0;
+        while (accumulate < 2_459_000){
+            accumulate = accumulate + save;
+            months += 1;
+            System.out.println("Месяц "+ months + ", сумма накоплений равна " + accumulate +  " рублей");
+        }
     }
 }
