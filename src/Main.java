70ad764047b0377542d4dfd16210e42bfc27da47
vlj2007 <vlj2007@yandex.ru>
2023-07-29 00:01:08 +0700

    I_have_finished_task3

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         task1();
         task2();
+        task3();
     }
 
     public static void task1() {
@@ -28,6 +29,46 @@ public class Main {
         for (; m > 0; m--) {
             System.out.print(m + " ");
         }
+        System.out.println();
+    }
 
+    public static void task3() {
+        System.out.println("Задача 3");
+        int countryY = 12_000_000;
+        int birthRate = 17;
+        int mortality = 8;
+        int totalPopulation = 0;
+        int yearBirthRate = 0;
+        int yearMortality = 0;
+
+//        int totalPopulationBirthRate = 0;
+        //        Год …, численность населения составляет …
+        while (totalPopulation < 10) {
+            totalPopulation = countryY * birthRate;
+            int totalPopulationBirthRate = totalPopulation / 1000;
+//            System.out.println("Рождаемость в 1 год " + totalPopulationBirthRate);
+
+            totalPopulation = countryY * mortality;
+            int totalPopulationMortality = totalPopulation / 1000;
+//            System.out.println("Смертность в 1 год " + totalPopulationMortality);
+
+            for (int i = 1; i <= 10; i++) {
+                yearBirthRate = totalPopulationBirthRate * i;
+//                System.out.println(i + " " + yearBirthRate);
+            }
+
+            for (int i = 1; i <= 10; i++) {
+                yearMortality = totalPopulationMortality * i;
+//                System.out.println(i + " " + yearMortality);
+            }
+
+            for (int i = 1; i <= 10; i++) {
+                int people = yearBirthRate - yearMortality;
+                int result = people * i;
+                System.out.println("Год " + i + ", численность населения составляет " + result);
+
+            }
+
+        }
     }
 }
